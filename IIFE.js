84d813e8e8627680cff632b98426e54bb2382bfc
Ali Yahya <165564206+CodebyAliyah@users.noreply.github.com>
2024-07-17 11:02:06 -0700

    Code Successfully

---
+++ IIFE.js
@@ -0,0 +1,19 @@
+
+// ** Immediately Invoked Function Expression ** //
+
+(function Chai () {
+  // IIFE Named.
+  console.log(`Hello Ali Yahya bright your future As a MERN Stack Developer`)
+}) () ;
+
+// () one paranthisis used for IIFE & other () Paranthesis used for function execute.
+
+// let's try on arrow function
+
+(aurCode = (name) => {
+  console.log(`Hello ${name} bright your future As a MERN Stack Developer`)
+}) ("Ali Yahya")
+
+
+
+
